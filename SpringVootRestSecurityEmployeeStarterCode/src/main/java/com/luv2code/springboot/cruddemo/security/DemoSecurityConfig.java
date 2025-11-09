4b847a532f618b9b654a5cff37bbb282a2e4dbdc
Kunal Chauhan <kunalchauhanlion@gmail.com>
2025-11-09 15:02:56 +0530

    Added Spring Security (Hardcoded)

--- SpringVootRestSecurityEmployeeStarterCode/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ SpringVootRestSecurityEmployeeStarterCode/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -28,6 +32,31 @@ public class DemoSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configuer ->
+                configuer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        //                                         /api/employees/{EmployeeId}
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
+                        //                                            /api/employees/{EmployeeId}
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable csrf
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+
+    }
+
 }
 
 
