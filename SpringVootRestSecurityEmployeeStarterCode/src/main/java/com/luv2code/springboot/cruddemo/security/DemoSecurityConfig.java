ce315f1013d1f27b8fbf51c590721f91bb10ffc7
Kunal Chauhan <kunalchauhanlion@gmail.com>
2025-11-09 15:15:17 +0530

    Added Spring Security now reading from the database

--- SpringVootRestSecurityEmployeeStarterCode/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ SpringVootRestSecurityEmployeeStarterCode/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,36 +1,84 @@
 package com.luv2code.springboot.cruddemo.security;
 
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class DemoSecurityConfig {
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails john = User.builder()
+//                .username("jhon")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE" , "MANAGER")
+//                .build();
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE" , "MANAGER" , "ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(john,mary,susan);
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        http.authorizeHttpRequests(configuer ->
+//                configuer
+//                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+//                        //                                         /api/employees/{EmployeeId}
+//                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
+//                        //                                            /api/employees/{EmployeeId}
+//                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+//        );
+//
+//        // use HTTP Basic authentication
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        // disable csrf
+//        http.csrf(csrf -> csrf.disable());
+//
+//        return http.build();
+//
+//    }
+//
+//}
+//
+//
+//                                                 Making security using DB
+//                                                 No more hardCoded User
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("jhon")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE" , "MANAGER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE" , "MANAGER" , "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(john,mary,susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource); // making a new user detail manager from the data source
     }
 
     @Bean
@@ -50,22 +98,10 @@ public class DemoSecurityConfig {
         // use HTTP Basic authentication
         http.httpBasic(Customizer.withDefaults());
 
-        // disable csrf
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf->csrf.disable());
 
         return http.build();
 
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
