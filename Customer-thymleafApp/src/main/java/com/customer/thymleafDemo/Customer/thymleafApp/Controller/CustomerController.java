f15f0e8523355b01436f7043687516431b8816c2
Kunal Chauhan <kunalchauhanlion@gmail.com>
2025-12-02 23:56:47 +0530

    made validation for max and min

--- Customer-thymleafApp/src/main/java/com/customer/thymleafDemo/Customer/thymleafApp/Controller/CustomerController.java
+++ Customer-thymleafApp/src/main/java/com/customer/thymleafDemo/Customer/thymleafApp/Controller/CustomerController.java
@@ -2,15 +2,26 @@ package com.customer.thymleafDemo.Customer.thymleafApp.Controller;
 
 import com.customer.thymleafDemo.Customer.thymleafApp.Model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder                                                        // called on every web Request
+    public void initBinderMethod(WebDataBinder dataBinder){            // get the data binder
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
+
     @GetMapping("/")
     public String showForm (Model themodel){
 
--- Customer-thymleafApp/src/main/java/com/customer/thymleafDemo/Customer/thymleafApp/Model/Customer.java
+++ Customer-thymleafApp/src/main/java/com/customer/thymleafDemo/Customer/thymleafApp/Model/Customer.java
@@ -1,5 +1,7 @@
 package com.customer.thymleafDemo.Customer.thymleafApp.Model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,18 @@ public class Customer {
     @NotNull(message = "This field is required!")
     @Size(min = 1 , message = "This field should have length of minimum of 1")
     private String lastName;
+
+    @Min(value = 0,message = "The Value should be greater or equal than 0")
+    @Max(value = 10 , message = "the value should be less than or equal to 10")
+    private int pass;
+
+    public int getPass() {
+        return pass;
+    }
+
+    public void setPass(int pass) {
+        this.pass = pass;
+    }
 
     public String getFirstName() {
         return firstName;
